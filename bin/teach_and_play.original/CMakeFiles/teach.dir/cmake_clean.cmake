58a1f318d8ed64c67e1c1c581ce2ff55eff720a0
Daniel M. Troniak <troniak@cs.ubc.ca>
2012-10-24 14:19:00 -0400

    adding teach_and_play code

---
+++ bin/teach_and_play.original/CMakeFiles/teach.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/teach.dir/teach.cpp.o"
+  "teach.pdb"
+  "teach"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/teach.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ bin/teach_and_play/CMakeFiles/play.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/play.dir/play.cpp.o"
+  "play.pdb"
+  "play"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/play.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
